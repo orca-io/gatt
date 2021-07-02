34cfe1aef091efa54fbb07dd0dee454cdc4869a9
Jon Bright <jon@siliconcircus.com>
2021-07-02 16:41:32 +0200

    More fork changes

--- device_linux.go
+++ device_linux.go
@@ -4,8 +4,8 @@ import (
 	"encoding/binary"
 	"net"
 
-	"github.com/bettercap/gatt/linux"
-	"github.com/bettercap/gatt/linux/cmd"
+	"github.com/Jon-Bright/gatt/linux"
+	"github.com/Jon-Bright/gatt/linux/cmd"
 )
 
 type device struct {
--- peripheral_linux.go
+++ peripheral_linux.go
@@ -10,7 +10,7 @@ import (
 	"net"
 	"strings"
 
-	"github.com/bettercap/gatt/linux"
+	"github.com/Jon-Bright/gatt/linux"
 )
 
 type peripheral struct {
